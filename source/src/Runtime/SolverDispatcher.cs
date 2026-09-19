301a3ebb78abff2b6b19fde411eeda9757425841
WUHU <xwr20070408@gmail.com>
2026-09-19 14:24:32 +0800

    refactor: keep multiplayer probe lifecycle wiring minimal

--- source/src/Runtime/SolverDispatcher.cs
+++ source/src/Runtime/SolverDispatcher.cs
@@ -1,7 +1,6 @@
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using Godot;
-using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Nodes;
 
 namespace CombatSolver;
@@ -48,11 +47,6 @@ internal sealed partial class SolverDispatcher : Node
             }
         }
         SolverController.MonitorCombatPresence();
-        if (CombatManager.Instance.IsInProgress
-            && CombatManager.Instance.DebugOnlyGetState() is CombatState combat)
-        {
-            MultiplayerClientProbe.Observe(combat, "main_frame");
-        }
         SolverController.RefreshSearchProgress();
         if (PerformanceRecording.Enabled)
             PerformanceRecording.Dispatcher(Stopwatch.GetElapsedTime(now).TotalMilliseconds,
