3b4461a551b9b0c6d4f5f01e371c25452fc43145
xwr20070408-cloud <xwr20070408@gmail.com>
2026-09-21 08:51:38 +0800

    Maintain history counters incrementally

--- source/src/Search/CombatHistoryCounterKey.cs
+++ source/src/Search/CombatHistoryCounterKey.cs
@@ -1,8 +1,6 @@
 using System.Collections.Frozen;
 using CombatSolver.Engine.InCombat.Simulation;
-using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Players;
-using MegaCrit.Sts2.Core.Models.Orbs;
 
 namespace CombatSolver;
 
@@ -27,47 +25,16 @@ internal static class CombatHistoryCounterKey
     }
 
     public static void Append(ref StateFingerprintBuilder key, CombatPredictionSimulator simulator, Player owner)
+        => AppendCounters(ref key, simulator.History.GetCounters(owner));
+
+    internal static void AppendCounters(ref StateFingerprintBuilder key, CombatHistoryCounters counters)
     {
-        Creature ownerCreature = owner.Creature;
-        int finishedPlays = 0;
-        int etherealPlays = 0;
-        int lightningChannels = 0;
-        int unblockedHitsReceived = 0;
-        int cardsDrawn = 0;
-        int cardsGenerated = 0;
-        foreach (CombatPredictionHistoryEntry entry in simulator.History)
-        {
-            switch (entry)
-            {
-                case CombatPredictionCardPlayFinishedEntry play:
-                    finishedPlays++;
-                    if (play.WasEthereal && play.CardPlay.Player == owner)
-                        etherealPlays++;
-                    break;
-                case CombatPredictionOrbChanneledEntry channel:
-                    if (channel.Orb is LightningOrb && channel.Orb.Owner == owner)
-                        lightningChannels++;
-                    break;
-                case CombatPredictionDamageReceivedEntry damage:
-                    if (damage.Receiver == ownerCreature && damage.Result.UnblockedDamage > 0)
-                        unblockedHitsReceived++;
-                    break;
-                case CombatPredictionCardDrawnEntry drawn:
-                    if (drawn.Card.Owner == owner)
-                        cardsDrawn++;
-                    break;
-                case CombatPredictionCardGeneratedEntry generated:
-                    if (generated.Creator == owner)
-                        cardsGenerated++;
-                    break;
-            }
-        }
         key.Add('h');
-        key.Add(finishedPlays);
-        key.Add(etherealPlays);
-        key.Add(lightningChannels);
-        key.Add(unblockedHitsReceived);
-        key.Add(cardsDrawn);
-        key.Add(cardsGenerated);
+        key.Add(counters.FinishedPlays);
+        key.Add(counters.EtherealPlays);
+        key.Add(counters.LightningChannels);
+        key.Add(counters.UnblockedHitsReceived);
+        key.Add(counters.CardsDrawn);
+        key.Add(counters.CardsGenerated);
     }
 }
